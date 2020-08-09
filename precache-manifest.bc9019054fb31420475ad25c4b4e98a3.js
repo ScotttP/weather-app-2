e6fb9df272733374741f1c45675492e60073b7c4
Scott Pribe <Scottpribe@gmail.com>
2020-08-08 20:37:25 -0400

    Updates

---
+++ precache-manifest.bc9019054fb31420475ad25c4b4e98a3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7af725fdc60fc5c77a5a02215338d97b",
+    "url": "/ScotttP/weather-app-2/index.html"
+  },
+  {
+    "revision": "b2b323d4bae6982e5f55",
+    "url": "/ScotttP/weather-app-2/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ab13aabfc19e6e0ce904",
+    "url": "/ScotttP/weather-app-2/static/js/2.4f39bcba.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ScotttP/weather-app-2/static/js/2.4f39bcba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b2b323d4bae6982e5f55",
+    "url": "/ScotttP/weather-app-2/static/js/main.98703de2.chunk.js"
+  },
+  {
+    "revision": "c2d7f75e7e8133d7c1df",
+    "url": "/ScotttP/weather-app-2/static/js/runtime-main.7c428ddc.js"
+  },
+  {
+    "revision": "be623d1679b713fdb8ac43654b546b79",
+    "url": "/ScotttP/weather-app-2/static/media/keys.be623d16.env"
+  }
+]);
