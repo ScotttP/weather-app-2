77b4d76ec0268d0683c69e8c6e2caa13240c2839
Scott Pribe <Scottpribe@gmail.com>
2020-08-10 22:07:31 -0400

    commit with prettier

--- weather-app-2/src/App.js
+++ weather-app-2/src/App.js
@@ -1,86 +1,76 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-import MainDisplay from './mainDisplay'
-import UserInput from './userInput'
+import MainDisplay from "./mainDisplay";
+import UserInput from "./userInput";
 
+export default class App extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			userInput: "New York",
+			icon: "",
+			city: "",
+			country: "US",
+			temp: "",
+			description: "",
+			feelsLike: "",
+			humidity: "",
+			windDirection: "",
+			windSpeed: "",
+			error: "",
+		};
 
+		this.handleChange = this.handleChange.bind(this);
+		this.handleSubmit = this.handleSubmit.bind(this);
+	}
 
+	componentDidMount() {
+		//calls the function on load
+		this.getWeatherData();
+	}
 
-export default class App extends React.Component{
-  constructor(props){
-    super(props)
-    this.state = {
-      userInput: 'New York',
-      icon: '',
-      city: '',
-      country: 'US',
-      temp: '',
-      description: '',
-      feelsLike: '',
-      humidity: '',
-      windDirection: '',
-      windSpeed: '',
-      error: ''
-      
-    }
+	async getWeatherData() {
+		try {
+			const response = await fetch(
+				`https://api.openweathermap.org/data/2.5/weather?q=${this.state.userInput}&APPID=b388badf2ea8d6a1b0fc28f7d99e0ccc`,
+				{ mode: "cors" }
+			);
+			const weatherData = await response.json();
+			this.setState({
+				city: weatherData.name,
+				icon: weatherData.weather[0].icon,
+				country: weatherData.sys.country,
+				temp: weatherData.main.temp,
+				description: weatherData.weather[0].description,
+				feelsLike: weatherData.main.feels_like,
+				humidity: weatherData.main.humidity,
+				windDirection: weatherData.wind.deg,
+				windSpeed: weatherData.wind.speed,
+				error: "",
+			});
+		} catch (error) {
+			alert("Could Not Find This City. Please enter a (City, Country)");
+		}
+	}
 
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
+	handleSubmit = (e) => {
+		e.preventDefault();
+		this.getWeatherData();
+	};
+	handleChange = (e) => {
+		this.setState({
+			userInput: e.target.value,
+		});
+		e.preventDefault();
+	};
 
-  componentDidMount(){ //calls the function on load
-    this.getWeatherData()
-    
-  }
-
-  async getWeatherData () {
-    try {
-        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${this.state.userInput}&APPID=b388badf2ea8d6a1b0fc28f7d99e0ccc`,{mode: 'cors'});
-        const weatherData = await response.json();
-        this.setState({
-          city : weatherData.name,
-          icon: weatherData.weather[0].icon,
-          country: weatherData.sys.country,
-          temp : weatherData.main.temp,
-          description: weatherData.weather[0].description,
-          feelsLike: weatherData.main.feels_like,
-          humidity: weatherData.main.humidity,
-          windDirection: weatherData.wind.deg,
-          windSpeed: weatherData.wind.speed,
-          error : ''
-        })  
-    } catch (error){
-      alert('Could Not Find This City. Please enter a (City, Country)')
-
-    }
-  }
-
-  handleSubmit = (e) => {
-    e.preventDefault()
-    this.getWeatherData()
-    
-  }
-  handleChange = (e) => {
-    this.setState({
-        userInput: e.target.value,
-    })
-    e.preventDefault()
-    
-  }
-  
-
-  render(error) {
-    
-      return (
-        <div className="App">
-        <UserInput click={this.handleSubmit} change={this.handleChange} />
-        <MainDisplay weather={this.state}/>
-        
-        </div>
-      )
-    
-    
-    
-  }
+	render(error) {
+		return (
+			<div className="App">
+				<UserInput click={this.handleSubmit} change={this.handleChange} />
+				<MainDisplay weather={this.state} />
+			</div>
+		);
+	}
 }
--- weather-app-2/src/mainDisplay.js
+++ weather-app-2/src/mainDisplay.js
@@ -1,48 +1,86 @@
-import React from 'react'
-
+import React from "react";
 
 export default class MainDisplay extends React.Component {
-    constructor(props){
-        super(props)
-        this.state = {
-          
-          
-        }
-    }
-    fahrenheit (temp) {
-        return Math.round((temp - 273.15)*9/5 +32 )
-        
-    }
-    mph (speed){
-        return Math.round(speed*2.23694*10)/10;
-
-    }
-    direction(wind) {
-        var val = Math.floor((wind / 22.5) + 0.5);
-        var arr = ["N", "NNE", "NE", "ENE", "E", "ESE", "SE", "SSE", "S", "SSW", "SW", "WSW", "W", "WNW", "NW", "NNW"];
-        return arr[(val % 16)];
-    }
-    render(){
-        
-        return(
-
-            <div id="contentContainer">
-                <div id="content">
-                    <div id="main">
-                        <h1 className="mainContent" id="location">{this.props.weather.city}, {this.props.weather.country}</h1>
-                        <img className="mainContent" id ="weatherIcon" src = {`http://openweathermap.org/img/wn/${this.props.weather.icon}@2x.png`} alt={this.props.weather.description + ' Icon'}/>
-                        <p id="temp"><strong >{ <>{this.fahrenheit(this.props.weather.temp)}&deg;F </>}</strong></p>
-                        <p id="weatherDescription"><strong >{this.props.weather.description}</strong></p>
-                    </div>
-                    <div id="details">
-                        <p className="detailContent" id="feelsLike">Feels Like: <strong>{ <>{this.fahrenheit(this.props.weather.temp)}&deg;F</>} </strong></p>
-                        <p className="detailContent" id="humidity">Humidity: <strong>{this.props.weather.humidity}%</strong></p>
-                        <p className="detailContent" id="windDirection">Wind Direction: <strong>{ <>{this.direction(this.props.weather.windDirection)}</> }</strong></p>
-                        <p className="detailContent" id="windSpeed">Wind Speed: <strong>{ <>{this.mph(this.props.weather.windSpeed)} MPH</>}</strong></p>
-                    </div>
-
-                </div>
-            </div>
-        )
-    }
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  fahrenheit(temp) {
+    return Math.round(((temp - 273.15) * 9) / 5 + 32);
+  }
+  mph(speed) {
+    return Math.round(speed * 2.23694 * 10) / 10;
+  }
+  direction(wind) {
+    var val = Math.floor(wind / 22.5 + 0.5);
+    var arr = [
+      "N",
+      "NNE",
+      "NE",
+      "ENE",
+      "E",
+      "ESE",
+      "SE",
+      "SSE",
+      "S",
+      "SSW",
+      "SW",
+      "WSW",
+      "W",
+      "WNW",
+      "NW",
+      "NNW",
+    ];
+    return arr[val % 16];
+  }
+  render() {
+    return (
+      <div id="contentContainer">
+        <div id="content">
+          <div id="main">
+            <h1 className="mainContent" id="location">
+              {this.props.weather.city}, {this.props.weather.country}
+            </h1>
+            <img
+              className="mainContent"
+              id="weatherIcon"
+              src={`http://openweathermap.org/img/wn/${this.props.weather.icon}@2x.png`}
+              alt={this.props.weather.description + " Icon"}
+            />
+            <p id="temp">
+              <strong>
+                {<>{this.fahrenheit(this.props.weather.temp)}&deg;F </>}
+              </strong>
+            </p>
+            <p id="weatherDescription">
+              <strong>{this.props.weather.description}</strong>
+            </p>
+          </div>
+          <div id="details">
+            <p className="detailContent" id="feelsLike">
+              Feels Like:{" "}
+              <strong>
+                {<>{this.fahrenheit(this.props.weather.temp)}&deg;F</>}{" "}
+              </strong>
+            </p>
+            <p className="detailContent" id="humidity">
+              Humidity: <strong>{this.props.weather.humidity}%</strong>
+            </p>
+            <p className="detailContent" id="windDirection">
+              Wind Direction:{" "}
+              <strong>
+                {<>{this.direction(this.props.weather.windDirection)}</>}
+              </strong>
+            </p>
+            <p className="detailContent" id="windSpeed">
+              Wind Speed:{" "}
+              <strong>
+                {<>{this.mph(this.props.weather.windSpeed)} MPH</>}
+              </strong>
+            </p>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
